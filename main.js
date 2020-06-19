463515e457a2e1f0935179d8605b9d721bc52f7e
Michael <michaelvini62@gmail.com>
2020-06-19 17:45:00 -0300

    ex1 js es6

--- main.js
+++ main.js
@@ -29,23 +29,57 @@
 
 // console.log(greenButton.element());
 
-class Button {
-    constructor(text, color){
-        this.text = text;
-        this.background = color; 
+// class Button {
+//     constructor(text, color){
+//         this.text = text;
+//         this.background = color; 
+//     }
+//     element(){
+//         const buttonElement = document.createElement('button');
+//         buttonElement.innerText = this.text;
+//         buttonElement.style.background = this.background;
+//         return buttonElement;
+//     }
+//     appendTo(target){
+//         const targetElement = document.querySelector(target);
+//         targetElement.appendChild(this.element());
+//         return targetElement;
+//     }
+// }
+
+// const blueButton = new Button('Pesquisar', 'blue', 'body');
+// console.log(blueButton.appendTo('body'));
+
+/* Exercicios js es6 */
+
+// // 1º Ex:
+
+class User {
+    constructor(email, senha){
+        this.email = email;
+        this.senha = senha;
     }
-    element(){
-        const buttonElement = document.createElement('button');
-        buttonElement.innerText = this.text;
-        buttonElement.style.background = this.background;
-        return buttonElement;
-    }
-    appendTo(target){
-        const targetElement = document.querySelector(target);
-        targetElement.appendChild(this.element());
-        return targetElement;
+    isAdm(){
+        return false;
     }
 }
 
-const blueButton = new Button('Pesquisar', 'blue', 'body');
-console.log(blueButton.appendTo('body'));
+class Admin extends User {
+    constructor(email, senha){
+        super(email,senha);
+    }
+    isAdm() {
+        return true;
+    }
+}
+
+const User1 = new User('email@teste.com','senha123');
+const Adm1 = new Admin ('email@teste.com','senha123');
+
+console.log(User1.email, User1.senha);
+console.log(User1.isAdm());
+
+console.log(Adm1.email, Adm1.senha);
+console.log(Adm1.isAdm());
+
+
